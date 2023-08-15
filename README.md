be7e60f0ccf2699bc6668326132596d5ffbd2713
Bence <m.bence211@gmail.com>
2023-08-15 19:53:47 +0200

    from scrimba to git

---
+++ README.md
@@ -0,0 +1,3 @@
+# co4c542748860044c52c90fa8
+
+This is my solution for the Scrimba React Router 6 course.
---
+++ api.js
@@ -0,0 +1,98 @@
+import { initializeApp } from "firebase/app";
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	doc,
+	getDoc,
+	query,
+	where,
+} from "firebase/firestore/lite";
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_apiKey,
+	authDomain: import.meta.env.VITE_authDomain,
+	projectId: import.meta.env.VITE_projectId,
+	storageBucket: import.meta.env.VITE_storageBucket,
+	messagingSenderId: import.meta.env.VITE_messagingSenderId,
+	appId: import.meta.env.VITE_appId,
+};
+
+console.log(firebaseConfig.apiKey);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const vansCollectionRef = collection(db, "vans");
+
+export async function getVans() {
+	const querySnapshot = await getDocs(vansCollectionRef);
+	const dataArr = querySnapshot.docs.map((doc) => ({
+		...doc.data(),
+		id: doc.id,
+	}));
+	console.log(dataArr);
+	return dataArr;
+}
+
+export async function getVan(id) {
+	const docRef = doc(db, "vans", id);
+	const vanSnapshot = await getDoc(docRef);
+	return {
+		...vanSnapshot.data(),
+		id: vanSnapshot.id,
+	};
+}
+
+export async function getHostVans() {
+	const q = query(vansCollectionRef, where("hostId", "==", "123"));
+	const querySnapshot = await getDocs(q);
+	const dataArr = querySnapshot.docs.map((doc) => ({
+		...doc.data(),
+		id: doc.id,
+	}));
+	return dataArr;
+}
+
+/* export async function getVans(id) {
+	const url = id ? `/api/vans/${id}` : "/api/vans";
+	const res = await fetch(url);
+	if (!res.ok) {
+		throw {
+			message: "Failed to fetch vans",
+			statusText: res.statusText,
+			status: res.status,
+		};
+	}
+	const data = await res.json();
+	return data.vans;
+}
+
+export async function getHostVans(id) {
+	const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
+	const res = await fetch(url);
+	if (!res.ok) {
+		throw {
+			message: "Failed to fetch vans",
+			statusText: res.statusText,
+			status: res.status,
+		};
+	}
+	const data = await res.json();
+	return data.vans;
+} */
+
+export async function loginUser(creds) {
+	const res = await fetch("/api/login", {
+		method: "post",
+		body: JSON.stringify(creds),
+	});
+	const data = await res.json();
+
+	if (!res.ok) {
+		throw {
+			message: data.message,
+			statusText: res.statusText,
+			status: res.status,
+		};
+	}
+
+	return data;
+}
